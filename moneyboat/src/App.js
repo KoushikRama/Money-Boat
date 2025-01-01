1a79c47329f9d2f0c05ceca585cd0fd5d2237135
KoushikRama <krama1673.pro@gmail.com>
2024-12-31 19:35:26 -0500

    made login page as default

--- moneyboat/src/App.js
+++ moneyboat/src/App.js
@@ -11,6 +11,7 @@ function App() {
       <div className="MoneyBoat">
         <Header />
         <Routes>
+          <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Routes>
